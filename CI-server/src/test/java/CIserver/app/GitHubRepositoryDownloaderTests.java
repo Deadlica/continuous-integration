e9563121e6cf40867e2f582e5b522a76a6767d26
Deadlica <samuelgreenberg00@gmail.com>
2024-02-11 22:27:35 +0100

    #14 feat: added unit test for the download method - Unit tests verifies that a zip file of the repository exists after GitHubRepositoryDownloader.download() has been executed

---
+++ CI-server/src/test/java/CIserver/app/GitHubRepositoryDownloaderTests.java
@@ -0,0 +1,37 @@
+package CIserver.app;
+
+import okhttp3.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(classes = GitHubRepositoryDownloader.class)
+class GitHubRepositoryDownloaderTests {
+
+    /**
+     * Tests the downloadRepository method of GitHubRepositoryDownloader and verifies
+     * that it can successfully download the repository of a given commit id
+     * @throws IOException GitHubRepositoryDownloader can throw IOException
+     */
+    @Test
+    void downloadRepository() throws IOException {
+        String commit = "4ed227fa48fc9106d176ed74d797484d5fa24790";
+        String owner = "Deadlica";
+        String repo = "continuous-integration";
+
+        GitHubRepositoryDownloader gitHubRepositoryDownloader = new GitHubRepositoryDownloader();
+        gitHubRepositoryDownloader.download(commit, owner, repo);
+
+        File zipFile = new File("repository.zip");
+        assert zipFile.exists();
+    }
+}
